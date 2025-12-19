2ce0c18262e56b50a5b1a3dbf0c8a0fb05e11452
tiffany <tiffanywang@berkeley.edu>
2025-12-19 15:27:31 -0800

    upgrade

--- components/Editor/Branchbar/index.tsx
+++ components/Editor/Branchbar/index.tsx
@@ -10,7 +10,10 @@ export default function Branchbar() {
     <div className="flex items-center gap-2 text-sm font-mono">
       <div
         onClick={() => setViewingBranch(null)}
-        className="bg-mint w-full bordered rounded-lg flex items-center justify-between px-3 py-2"
+        className="w-full bordered rounded-lg flex items-center justify-between px-3 py-2"
+        style={{
+          background: 'linear-gradient(180deg, var(--color-mint) 0%, var(--color-lime) 100%)',
+        }}
       >
         <div className="flex items-center justify-center">
           <Branch seed={randomSeed} />
